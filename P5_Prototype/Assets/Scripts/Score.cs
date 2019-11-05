59299cea37dc1e9a054e59104fcbdd8578263fde
Avalab A <Avalab@create.aau.dk>
2019-11-05 09:28:36 +0100

    ui

---
+++ P5_Prototype/Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public Gameobject Canvas;
+    private image progressbar;
+    private text Score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
